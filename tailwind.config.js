5cbd5334381819a2c998e79b308bac0435df16fa
Loris Galler <gallerloris@gmail.com>
2024-06-13 22:53:09 +0200

    animation added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,18 @@ module.exports = {
     "./index_tailwind.html",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fadeOut: {
+          '0%': { opacity: '1' },
+          '100%': { opacity: '0' },
+        }
+      },
+      animation: {
+        fadeOut: 'fadeOut 0.5s forwards',
+      }
+    },
   },
-  plugins: [],
+  plugins: []
 }
 
