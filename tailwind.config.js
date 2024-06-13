f465a4665cdee28263fa960f8d6ecda4a4c10764
Loris Galler <gallerloris@gmail.com>
2024-06-13 19:16:13 +0200

    installed with npm

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: [
+    "./index_tailwind.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
